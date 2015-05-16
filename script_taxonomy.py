cd209b2b76a544a367860e820ef98b0459b29b0e
johannakate <jkgriffin@ucdavis.edu>
2015-05-15 17:50:25 -0700

    working code for script_taxonomy.py

--- script_taxonomy.py
+++ script_taxonomy.py
@@ -15,31 +15,42 @@ DB_URI = "http:///taxonomy.db"
 
 def get_wm_taxonomy():
 	"""This connects to WM API taxonomy and returns the total list of taxonomy information for their database"""
-	req = urllib2.Request('http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q') 
-	r = urllib2.urlopen(req) 
-	taxonomy_dict = json.loads(r)
+	# req = urllib2.Request('http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q')
+	# for line in req[:10]:
+	# 	print line 
+	r = requests.get('http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q') 
+	taxonomy_dict = r.json()
+	# print test
 	a = taxonomy_dict['categories']
-	recurse_keys(a)
+	# a is a list object of dictionaries
+	b = a[0]
+	# print b
+	recurse_keys(b)
 
 
-def recurse_keys(a):
-    for key in a:
-        id = key(['id'])
-        name = key(['name'])
-        path = key(['path'])
-        if 'children' in key:
-        	children = True
-        	recurse_keys(key['children'])
-		taxonomy_table_values = Taxonomy(children=children, category_node=category["id"], name=category["name"], path=category["path"])
-		db.session.add(taxonomy_table_values)
-		db.session.commit()
+def recurse_keys(b):
+    for key in b.keys():
+        cat_id = b['id']
+        name = b['name']
+        path = b['path']
+        print cat_id, name, path
+#         if 'children' in key:
+#         	children = True
+#         # children: [{
+# # 
+#         # 
+#         # }]
+#         	recurse_keys(key['children'])
+# 		taxonomy_table_values = Taxonomy(children=children, category_node=category["id"], name=category["name"], path=category["path"])
+# 		db.session.add(taxonomy_table_values)
+# 		db.session.commit()
 
 # with open('taxonomy.json') as data_file:    
 #     data = json.load(data_file)
 #     a = data['categories']
 #     recurse_keys(a)
 
-# #####################################################################################################################
+#####################################################################################################################
 # def get_wm_taxonomy(categoryNode):
 # 	"""This connects to WM API taxonomy and returns the total list of taxonomy information for their database."""
 
