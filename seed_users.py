7bb0bb160c50e756a00ac51c030eec6877c9c15f
johannakate <jkgriffin@ucdavis.edu>
2015-05-19 16:38:40 -0700

    if name = main added

--- seed_users.py
+++ seed_users.py
@@ -8,12 +8,18 @@ DB_URI = "http:///taxonomy.db"
 
 def seed_users():
 	"""This takes the .csv user data and seeds it into my database"""
-	anything = "anything"
-	print "anything"
-	return anything
+
 	# users = csv.reader('/data/fake_user_data.csv', delimiter=' ', quotechar='"')
 	# print users
 	# user_table_values = User(name=name, email=email, password=password)
 	# db.session.add(taxonomy_table_values)
 
 	# db.session.commit()
+
+
+if __name__ == '__main__':
+	from server import app
+	connect_to_db(app)
+	print "Connected to DB."
+	get_wm_taxonomy()
+
