6f77e6f1502cd1ab4cc249961a280c4de5e30559
johannakate <jkgriffin@ucdavis.edu>
2015-05-18 21:46:46 -0700

    more working code pt IV

--- script.py
+++ script.py
@@ -10,46 +10,54 @@ api_key = "qb5mmbrawdsnnr74yqc6sn8q"
 
 
 dict_of_product_ids = {}
+
+def userSearch(searchTerm):
+	"""This is the function that passes the user search term to the Walmart API"""	
+	search_api = requests.get("http://api.walmartlabs.com/v1/search?query=" + searchTerm + "&format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q")
+	searchApi = search_api.json()
+	return searchApi
+
+	# print body
 # Lookup API
 
 	# user chooses product using search api, 
 	# pulls id from search api and provides lookup api info
-def showProductProfile():
-	"""Get the full product info from the Lookup API."""
+# def showProductProfile():
+# 	"""Get the full product info from the Lookup API."""
 
-	Lookup_api = requests.get("http://api.walmartlabs.com/v1/items/" + "%d" 
-	+ "?format=xml&apiKey=qb5mmbrawdsnnr74yqc6sn8q") % (
-	# product_id
-	) 
+# 	Lookup_api = requests.get("http://api.walmartlabs.com/v1/items/" + "%d" 
+# 	+ "?format=xml&apiKey=qb5mmbrawdsnnr74yqc6sn8q") % (
+# 	# product_id
+# 	) 
 
-	response = Lookup_api.read()
-	body = response
+# 	response = Lookup_api.read()
+# 	body = response
     
-	print body
+# 	print body
 
-product = requests.get('Lookup_api')
-
-
-def showProductRating():
-	"""Get Walmart's user's reviews on this product."""
-
-	Reviews_api = requests.get("http://api.walmartlabs.com/v1/reviews/" + "%d" 
-	+ "?format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q") % (
-	# product_id
-	)
-
-def searchProductsMyUser():
-	"""Search Walmart.com catalogue and return items available for sale.
-
-	Query by string values."""
-	search_api = requests.get("http://api.walmartlabs.com/v1/search?query=" 
-		+ "%s" 
-		+ "+%s"
-		# how do add additional search terms?
-		"&format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q")
-
-def searchProductsasDev():
-	"""Search with categoryNode"""
+# product = requests.get('Lookup_api')
 
 
-# Taxonomy API in script_taxonomy.py 
+# def showProductRating():
+# 	"""Get Walmart's user's reviews on this product."""
+
+# 	Reviews_api = requests.get("http://api.walmartlabs.com/v1/reviews/" + "%d" 
+# 	+ "?format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q") % (
+# 	# product_id
+# 	)
+
+# def searchProductsMyUser():
+# 	"""Search Walmart.com catalogue and return items available for sale.
+
+# 	Query by string values."""
+# 	search_api = requests.get("http://api.walmartlabs.com/v1/search?query=" 
+# 		+ "%s" 
+# 		+ "+%s"
+# 		# how do add additional search terms?
+# 		"&format=json&apiKey=qb5mmbrawdsnnr74yqc6sn8q")
+
+# def searchProductsasDev():
+# 	"""Search with categoryNode"""
+
+
+# # Taxonomy API in script_taxonomy.py 
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 
 # from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, jsonify, json
 # as browser_session ????? 
 from flask_debugtoolbar import DebugToolbarExtension
-
+from script import userSearch
 from model import connect_to_db, db
 
 
@@ -17,6 +17,14 @@ def index():
 	"""Homepage."""
 
 	return render_template("index.html")
+
+@app.route('/search')
+def search():
+	"""This search returns Walmart API Search Items"""
+	search = request.args.get('userSearch')
+	results = userSearch(search)
+	return jsonify(results)
+
 
 
 if __name__ == "__main__":
