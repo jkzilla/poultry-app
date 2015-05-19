73def6c67afb5aec429b4441754a7722fd786ffe
johannakate <jkgriffin@ucdavis.edu>
2015-05-18 18:35:34 -0700

    angular js search started

---
+++ test/e2e/scenarios.js
@@ -0,0 +1,38 @@
+describe('ChickenWatch App', function () {
+
+	describe('Chicken product list view', function() {
+
+		beforeEach(function() {
+			browser.get('poultry_app/templates/index.html');
+		});
+
+		it('should filter the chicken product list as a
+			user types into the search box', function() {
+
+			var phoneList = element.all(by.repeater('chickenItem in chickenProducts'));
+			var query = element(by.model('query'));
+
+			expect(chickenProductList.
+
+				//tutorial:
+				 //chicken.ProductList.count()).toBe(3)
+				  //there are only 3 phones in the tutorial 
+				);
+
+			query.sendKeys(
+			 // tutorial: 'nexus'
+				)
+			expect(
+// 			tutorial: phoneList.count()).toBe(1);
+				);
+
+			query.clear();
+			query.sendKeys(
+// 			'motorola'
+				);
+			expect(
+// 			phoneList.count()).toBe(2
+				);
+			});
+	});
+});
